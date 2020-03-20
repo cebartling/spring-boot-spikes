44425c86bd7dc8e65de036dee1180509fa247149
Christopher Bartling <chris.bartling@gmail.com>
2020-03-19 20:12:33 -0500

    Initial Spring Initializr app

---
+++ spring-scheduling/src/main/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/SpringSchedulingApplication.kt
@@ -0,0 +1,11 @@
+package com.pintailconsultlingllc.spikes.spring.scheduling
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSchedulingApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSchedulingApplication>(*args)
+}
---
+++ spring-scheduling/src/test/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/SpringSchedulingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pintailconsultlingllc.spikes.spring.scheduling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSchedulingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
