a7ae223e7c669d1a71dd7b01503c09ec6dc67fd5
Christopher Bartling <chris.bartling@gmail.com>
2020-03-20 00:11:42 -0500

    Reformatting code.

--- spring-scheduling/src/main/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/jpa/repositories/PersonRepository.kt
+++ spring-scheduling/src/main/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/jpa/repositories/PersonRepository.kt
@@ -3,5 +3,6 @@ package com.pintailconsultlingllc.spikes.spring.scheduling.jpa.repositories
 import com.pintailconsultlingllc.spikes.spring.scheduling.jpa.entities.Person
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PersonRepository : JpaRepository<Person, Int> {
-}
+interface PersonRepository : JpaRepository<Person, Int>
+
+
