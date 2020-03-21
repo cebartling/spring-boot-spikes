26dcb78762eef5c5843e80cc29fa35b90fb2d82d
Christopher Bartling <chris.bartling@gmail.com>
2020-03-20 23:07:16 -0500

    Adding Async processing

--- spring-scheduling/src/main/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/SpringSchedulingApplication.kt
+++ spring-scheduling/src/main/kotlin/com/pintailconsultlingllc/spikes/spring/scheduling/SpringSchedulingApplication.kt
@@ -2,10 +2,12 @@ package com.pintailconsultlingllc.spikes.spring.scheduling
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 class SpringSchedulingApplication
 
 fun main(args: Array<String>) {
