06a4d38e4603fc059480fbf62a2621d709723af5
Christopher Bartling <chris.bartling@gmail.com>
2020-03-22 11:48:03 -0500

    Spring Boot Microservices with Spring Cloud Beginner to Guru: End of lecture 209. Sending events to state machine.

--- state-machine/src/main/kotlin/com/pintailconsultingllc/spring/spikes/statemachine/services/PaymentServiceImpl.kt
+++ state-machine/src/main/kotlin/com/pintailconsultingllc/spring/spikes/statemachine/services/PaymentServiceImpl.kt
@@ -4,10 +4,14 @@ import com.pintailconsultingllc.spring.spikes.statemachine.domain.PaymentEvent
 import com.pintailconsultingllc.spring.spikes.statemachine.domain.PaymentState
 import com.pintailconsultingllc.spring.spikes.statemachine.jpa.entities.Payment
 import com.pintailconsultingllc.spring.spikes.statemachine.jpa.repositories.PaymentRepository
+import org.springframework.messaging.Message
+import org.springframework.messaging.support.MessageBuilder
 import org.springframework.statemachine.StateMachine
 import org.springframework.statemachine.config.StateMachineFactory
 import org.springframework.statemachine.support.DefaultStateMachineContext
 import org.springframework.stereotype.Service
+
+const val PAYMENT_ID_HEADER_KEY = "payment_id"
 
 @Service
 class PaymentServiceImpl(
@@ -21,23 +25,34 @@ class PaymentServiceImpl(
     }
 
     override fun preAuthorizePayment(paymentId: Long): StateMachine<PaymentState, PaymentEvent> {
-        val payment = paymentRepository.getOne(paymentId)
+        val stateMachine = build(paymentId)
+        sendEvent(paymentId, stateMachine, PaymentEvent.PRE_AUTHORIZE)
         TODO("Not yet implemented")
     }
 
     override fun authorizePayment(paymentId: Long): StateMachine<PaymentState, PaymentEvent> {
-        val payment = paymentRepository.getOne(paymentId)
+        val stateMachine = build(paymentId)
+        sendEvent(paymentId, stateMachine, PaymentEvent.AUTHORIZE)
         TODO("Not yet implemented")
     }
 
     override fun declineAuth(paymentId: Long): StateMachine<PaymentState, PaymentEvent> {
-        val payment = paymentRepository.getOne(paymentId)
+        val stateMachine = build(paymentId)
+        sendEvent(paymentId, stateMachine, PaymentEvent.AUTH_DECLINED)
         TODO("Not yet implemented")
     }
 
     override fun approveAuth(paymentId: Long): StateMachine<PaymentState, PaymentEvent> {
-        val payment = paymentRepository.getOne(paymentId)
+        val stateMachine = build(paymentId)
+        sendEvent(paymentId, stateMachine, PaymentEvent.AUTH_APPROVED)
         TODO("Not yet implemented")
+    }
+
+    private fun sendEvent(paymentId: Long, stateMachine: StateMachine<PaymentState, PaymentEvent>, event: PaymentEvent) {
+        val message: Message<PaymentEvent> = MessageBuilder.withPayload(event)
+                .setHeader(PAYMENT_ID_HEADER_KEY, paymentId)
+                .build()
+        stateMachine.sendEvent(message)
     }
 
     private fun build(paymentId: Long): StateMachine<PaymentState, PaymentEvent> {
