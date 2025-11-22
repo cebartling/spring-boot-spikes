8fd33f93a3117e1f34f3920b3bb37745a575fc8e
Christopher Bartling <chris.bartling@gmail.com>
2025-11-22 10:23:11 -0600

    chore: creating new spike project

---
+++ cqrs-spike/src/main/kotlin/com/pintailconsultingllc/cqrsspike/CqrsSpikeApplication.kt
@@ -0,0 +1,11 @@
+package com.pintailconsultingllc.cqrsspike
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CqrsSpikeApplication
+
+fun main(args: Array<String>) {
+    runApplication<CqrsSpikeApplication>(*args)
+}
---
+++ cqrs-spike/src/test/kotlin/com/pintailconsultingllc/cqrsspike/CqrsSpikeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.cqrsspike
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CqrsSpikeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
